f3e5b29266e3ce5bf2e1c7883fe65294d4e7cafc
James Norman <jnorman@pivotal.io>
2019-06-18 17:49:21 +0100

    add TODO to listops

---
+++ list-ops/listops.go
@@ -0,0 +1,22 @@
+package listops
+
+type listops struct {
+}
+
+func (l *listops) foldl() {
+
+}
+
+// TODO
+// foldl
+// foldr
+// filter - returns a new array with all items which pass the specified criteria (function called on every item in the array.)
+// length
+// map - calls a method on every item in the array.
+// reverse
+// append
+// concat
+// Folding is the same as reduce in Javascript -
+// - Take an identity (accumulator, int/bool)
+// - Take an operation to perform on the identity / accumulated value
+// - And a list on which to iterate. return fold(false, or, list);
