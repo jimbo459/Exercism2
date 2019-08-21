86c9b6670bb9c0cd5d5b033aa24edc38d6628f03
James Norman <jnorman@pivotal.io>
2019-08-21 08:46:32 +0100

    list ops
    Signed-off-by: Mirah Gary <mgary@pivotal.io>

--- list-ops/listops.go
+++ list-ops/listops.go
@@ -1,22 +1,9 @@
 package listops
 
-type listops struct {
+type IntList []int
+
+func (l *IntList) foldl(f func(x,y int) int , in int) int {
+	
+	
+	return 
 }
-
-func (l *listops) foldl() {
-
-}
-
-// TODO
-// foldl
-// foldr
-// filter - returns a new array with all items which pass the specified criteria (function called on every item in the array.)
-// length
-// map - calls a method on every item in the array.
-// reverse
-// append
-// concat
-// Folding is the same as reduce in Javascript -
-// - Take an identity (accumulator, int/bool)
-// - Take an operation to perform on the identity / accumulated value
-// - And a list on which to iterate. return fold(false, or, list);
